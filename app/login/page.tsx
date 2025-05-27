b735e7644ab49f4eb005f49f1a49995d9625492f
Kyan Cox <kyan.cox@gmail.com>
2025-05-27 05:24:37 -0400

    Supabase boilerplate auth

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+// app/login/page.tsx
+'use client'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { supabase } from '@/lib/supabaseClient'
+
+export default function LoginPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['google']}
+      />
+    </div>
+  )
+}
