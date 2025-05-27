b9275542bfce2f993b17b6959ddba0f8a92fb22a
Kyan Cox <kyan.cox@gmail.com>
2025-05-27 05:51:13 -0400

    bg-white for readability

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import { supabase } from '@/lib/supabaseClient'
 
 export default function LoginPage() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center bg-white">
       <Auth
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}
